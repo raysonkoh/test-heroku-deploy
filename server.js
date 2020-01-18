792f391b503450d897683c4d5fa370aeac5f3bc3
LOVELELESS <koh_rayson@yahoo.com.sg>
2020-01-19 06:17:18 +0800

    changed production to process

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-if (production.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
